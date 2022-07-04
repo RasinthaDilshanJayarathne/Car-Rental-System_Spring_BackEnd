2ff8b81510cdff800d0d586a68b0365eced6058d
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-07-04 13:48:47 +0530

    finished delete customer

---
+++ BackEnd/src/main/java/lk/ijse/easy/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package lk.ijse.easy.controller;
+
+import lk.ijse.easy.dto.CustomerDTO;
+import lk.ijse.easy.service.CustomerService;
+import lk.ijse.easy.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("customer")
+@CrossOrigin
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCustomers(){
+        return new ResponseUtil(200,"Successfully Loaded",customerService.getAllCustomers());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@RequestBody CustomerDTO customer){
+        customerService.saveCustomer(customer);
+        return new ResponseUtil(200,"Saved",null);
+
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCustomer(@RequestParam String id) {
+        customerService.deleteCustomer(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO customer){
+        customerService.updateCustomer(customer);
+        return new ResponseUtil(200,"Updated",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@PathVariable String id, @PathVariable String customerId) {
+        CustomerDTO customerDTO = customerService.searchCustomer(id);
+        return new ResponseUtil(200,"Found",customerDTO);
+    }
+}
